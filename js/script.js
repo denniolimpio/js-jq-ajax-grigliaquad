07e7ce2ff57c2d061b07977943d99e3945ac2c75
dennis <dennis.olimpio0794@gmail.com>
2020-06-25 15:42:22 +0200

    bux fix
    ho fatto in modo che ad ogni nuovo click venisse rimossa  al box la classe preesistente

--- js/script.js
+++ js/script.js
@@ -4,33 +4,41 @@ $(document).ready(function() {
 
   $(".box").click(  function() {
 
-
+    //dichiaro la mia variabile elemento cliccato
     var elementoCliccato = this;
-
 
     $.ajax({
 
       url: 'https://flynn.boolean.careers/exercises/api/random/int',
       method: 'GET',
 
-      success: function(data) {
+      success: function(numero) {
 
-        var numeroRandom = data.response;
+        var numeroRandom = numero.response;
 
         // output
 
         $(elementoCliccato).text(numeroRandom);
 
+        // ad ogni clicco rimuovo la classe preesistente
+
+        $(elementoCliccato).removeClass("yellow");
+        $(elementoCliccato).removeClass("green");
 
         // Se è <= 5 il quadrato diventa giallo,
+
         if ( numeroRandom <= 5) {
 
+          // aggiundo la classe yellow
           $(elementoCliccato).addClass("yellow");
 
           // se è > di 5 il quadrato diventa verde.
 
         } else {
+
+          // aggiundo la classe green
           $(elementoCliccato).addClass("green");
+
         }
 
 
